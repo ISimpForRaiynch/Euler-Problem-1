2ebf3f35f033a190ff5270c956918a740221d984
ISimpForRaiynch <87456452+ISimpForRaiynch@users.noreply.github.com>
2021-07-26 14:51:37 -0500

    Add files via upload
    Add Console.ReadLine(); To the end of the program.cs file if you want the exe to run "properly"

---
+++ Euler Project/Euler Project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Euler_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.WriteLine("Project Euler 1");
+            for (int i = 1; i < 1000; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    result += i;
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
